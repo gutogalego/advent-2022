1ac2d34fe4eb3767ced401e63cb0a3ea1677eaf1
Augusto Galego <augustomirandagalego@gmail.com>
2023-09-02 21:09:59 +0200

    .

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -2,12 +2,15 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    println!("Hello, world!");
+    solve1();
+    solve2();
+}
+
+fn solve1() {
     let relative_path = Path::new("input1.txt");
 
     let mut max_cal: i64 = 0;
     let mut current_cals = 0;
-
     for line in fs::read_to_string(relative_path)
         .expect("Unable to read file")
         .lines()
@@ -22,4 +25,28 @@ fn main() {
             current_cals += line.parse::<i64>().expect("Issue parsing");
         }
     }
+}
+
+fn solve2() {
+    let relative_path = Path::new("input1.txt");
+
+    let mut current_cals = 0;
+    let mut vec_elves: Vec<i64> = Vec::new();
+
+    for line in fs::read_to_string(relative_path)
+        .expect("Unable to read file")
+        .lines()
+    {
+        if line.is_empty() {
+            vec_elves.push(current_cals);
+            current_cals = 0;
+        } else {
+            current_cals += line.parse::<i64>().expect("Issue parsing");
+        }
+    }
+    vec_elves.sort();
+    vec_elves.reverse();
+    let last_three: i64 = vec_elves.iter().take(3).sum();
+    println!("Result: {:?}", last_three);
+
 }
