e7a88d8c2b99c764a00ef2e1fe3abc27dbaead8d
Augusto Galego <augustomirandagalego@gmail.com>
2023-09-19 18:56:50 +0200

    .

--- sandbox-2/src/main.rs
+++ sandbox-2/src/main.rs
@@ -5,7 +5,6 @@ struct Rectangle {
 }
 
 impl Rectangle {
-
     fn square(size: u32) -> Self {
         Self {
             width: size,
@@ -15,16 +14,19 @@ impl Rectangle {
 
     fn area(&self) -> u32 {
         self.width * self.height
-
     }
 
     fn area51(&mut self) {
-        self.width = self.width*2;
+        self.width = self.width * 2;
     }
 }
 
-
 fn main() {
+    //a();
+    b();
+}
+
+fn a() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
@@ -41,4 +43,97 @@ fn main() {
     let rect2 = Rectangle::square(3);
 
     dbg!(rect2);
+}
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            print!("Lucky penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn b() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}{:?}", six, none);
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let v: Vec<i32> = Vec::new();
+    let mut v = vec![1, 2, 3];
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    println!("V: {:?}", v);
+
+    println!("V3: {:?}", v[2]);
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+    for i in &mut v {
+        *i += 50;
+    }
+    for i in &v {
+        println!("{i}");
+    }
+
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
