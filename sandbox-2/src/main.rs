4ee7433ec16837610576584b85b00fa148a83487
Augusto Galego <augustomirandagalego@gmail.com>
2023-09-19 18:00:12 +0200

    .

--- sandbox-2/src/main.rs
+++ sandbox-2/src/main.rs
@@ -1,3 +1,44 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+
+    }
+
+    fn area51(&mut self) {
+        self.width = self.width*2;
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("rect1 is {:?}", rect1);
+    dbg!(rect1);
+
+    let rect2 = Rectangle::square(3);
+
+    dbg!(rect2);
 }
